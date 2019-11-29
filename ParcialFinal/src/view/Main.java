c7fbafc6c5708465e0d2c693d7fea84c7bac9dbf
Juan1899 <53906952+Juan1899@users.noreply.github.com>
2019-11-29 16:01:30 -0500

    Creación del proyecto.
    Se creó la clase main, sus métodos y se importó el core de Processing(PApplet)

---
+++ ParcialFinal/src/view/Main.java
@@ -0,0 +1,32 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main(Main.class.getName());
+
+	}
+
+	
+	public void settings() {
+		
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
